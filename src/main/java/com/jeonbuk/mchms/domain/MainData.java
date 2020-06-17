1f048d704c1240f4e44ddb50864023b359b91b15
CHAEUNPARK <codjs0910@naver.com>
2020-06-17 11:24:28 +0900

    ichngo Main

--- src/main/java/com/jeonbuk/mchms/domain/MainData.java
+++ src/main/java/com/jeonbuk/mchms/domain/MainData.java
@@ -11,6 +11,10 @@ public class MainData {
     String cgi_id;
     String subject;
     String title;
+    String contents;
     String image;
     String upload_date;
+    String author;
+    String ngo_name;
+    String cgi_name;
 }
